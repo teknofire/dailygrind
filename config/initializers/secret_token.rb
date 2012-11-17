df1f4e0bcc36ff87859c1ad5e2b607e63183021f
Will Fisher <will@teknofire.net>
2012-11-17 00:09:59 -0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dailygrind::Application.config.secret_token = '5f62995668969a03cd4b492de00dba8c78fced863aecd4d381f9d82d2a2e9f9e2c1db48cbadab3beff8e1c2213ea3c748ed0835870ccc07bcd6f4c6fb19e0f7d'
