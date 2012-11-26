17a82bb7bc7d90fe1c69803bb049563e191940e7
Will Fisher <will@teknofire.net>
2012-11-25 18:14:36 -0900

    use actual end of week date

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,7 @@ class WelcomeController < ApplicationController
     unless current_user.nil?
       @transaction = CoffeeCharge.new
       @start_date = Time.zone.now.beginning_of_week
-      @end_date = Time.zone.now
+      @end_date = Time.zone.now.end_of_week
     
       @drank_this_week = current_user.coffee_charges.where('created_at >= ?', Time.zone.now.beginning_of_week)
     end
