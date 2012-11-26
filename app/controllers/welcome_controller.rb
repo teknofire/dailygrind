3f539ee62e92bd5551efd6e9a41b228bb563f083
Will Fisher <will@teknofire.net>
2012-11-25 17:28:45 -0900

    fix issue with users not being logged in

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,11 @@
 class WelcomeController < ApplicationController
   def index
-    @transaction = CoffeeCharge.new
-    @start_date = Time.zone.now.beginning_of_week
-    @end_date = Time.zone.now
+    unless current_user.nil?
+      @transaction = CoffeeCharge.new
+      @start_date = Time.zone.now.beginning_of_week
+      @end_date = Time.zone.now
     
-    @drank_this_week = current_user.coffee_charges.where('created_at >= ?', Time.zone.now.beginning_of_week)
+      @drank_this_week = current_user.coffee_charges.where('created_at >= ?', Time.zone.now.beginning_of_week)
+    end
   end
 end
